8dc2a5632528b5f512cce0d5a333ffc9e5c5c97e
Tomas Carletto <tcarletto@fi.uba.ar>
2025-06-03 18:22:50 -0300

    Update db.sql

--- main/backend/db.sql
+++ main/backend/db.sql
@@ -6,7 +6,19 @@ CREATE TABLE Usuario(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL,
+    password_ VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE Stock(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    cantidad INT NOT NULL DEFAULT 0,
+    categoria ENUM('Juego de mesa', 'Juego de Cartas', 'Juego de rol') NOT NULL,
+    publico_destinado ENUM('Todo público', 'Adultos') NOT NULL,
+    precio DECIMAL(10,2) NOT NULL 
+
 );
