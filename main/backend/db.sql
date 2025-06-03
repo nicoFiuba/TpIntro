fefb07b8db5993e2a0d2a46d281ea55dec82bf3e
paste-fiuba <mpasteloff@fi.uba.ar>
2025-06-03 17:42:54 -0300

    Update db.sql

--- main/backend/db.sql
+++ main/backend/db.sql
@@ -6,6 +6,11 @@ CREATE TABLE Usuario(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
+<<<<<<< Updated upstream
     password_ VARCHAR(50) NOT NULL,
+=======
+    contraseña VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+>>>>>>> Stashed changes
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
